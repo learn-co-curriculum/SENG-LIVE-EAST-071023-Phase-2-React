ba4f6b4074ede2e0545abc2a709f3d0c9cb15a55
TR-Wolf <thomasrussellwolf@gmail.com>
2023-08-03 11:38:55 -0500

    lecture complete

--- 04_react_forms/project_showcase/src/components/ProjectForm.js
+++ 04_react_forms/project_showcase/src/components/ProjectForm.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ProjectForm() {
+const initalState = {
+  name: "",
+  about: "",
+  phase: "",
+  link: "",
+  image: ""
+}
+function ProjectForm({ addNewProject }) {
+  const [formData, setFormData] = useState(initalState);
+
+  const {name, about, phase, link, image} = formData
+
+  const handleOnChange = (event) => {
+    const {name, value} = event.target
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addNewProject(formData)
+    setFormData(initalState)
+  }
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input 
+          type="text" 
+          id="name" 
+          name="name"
+          value={name}
+          onChange={handleOnChange}
+      />
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea 
+        id="about" 
+        name="about"
+        value={about}
+        onChange={handleOnChange}
+      />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select 
+          id="phase"
+          name="phase"
+          value={phase}
+          onChange={handleOnChange}
+        >
           <option>Select One</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
@@ -23,10 +64,22 @@ function ProjectForm() {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input 
+          type="text" 
+          id="link" 
+          name="link"
+          value={link}
+          onChange={handleOnChange}
+          />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input 
+          type="text" 
+          id="image" 
+          name="image"
+          value={image}
+          onChange={handleOnChange}
+          />
 
         <button type="submit">Add Project</button>
       </form>
--- 04_react_forms/project_showcase/src/components/ProjectsContainer.js
+++ 04_react_forms/project_showcase/src/components/ProjectsContainer.js
@@ -12,9 +12,20 @@ function ProjectsContainer() {
       .then((projectsData) => setProjects(projectsData));
   }
 
+  const addNewProject = (project) => {
+    // setProjects((projects) => [...projects, project])
+    fetch("http://localhost:4000/projects", {
+
+    })
+      .then((res) => res.json())
+      .then((projectData) => setProjects([...[projects], projectData]));
+  }
+
   return (
     <>
-      <ProjectForm />
+      <ProjectForm 
+        addNewProject={addNewProject}
+      />
       <ProjectList
         onLoadProjects={onLoadProjects}
         projects={projects}
